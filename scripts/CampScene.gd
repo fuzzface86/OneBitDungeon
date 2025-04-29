b3d17f508044636f75194149d424fd200cccbd77
fuzzface86 <ianevertson@gmail.com>
2025-04-29 17:18:15 -0400

    Delete scripts directory

--- scripts/CampScene.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-func _ready():
-	var back_button = get_node("BackButton")
-	back_button.pressed.connect(_on_back_button_pressed)
-
-func _on_back_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
--- scripts/intro.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-@onready var anim_player: AnimationPlayer = $AnimationPlayer
-
-func _ready():
-	await anim_player.animation_finished
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
